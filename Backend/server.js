891162cc69c778f879dba4e9d24ae53c42db7936
Jeanette <aejlindberg@gmail.com>
2018-11-15 12:15:16 +0100

    MongoDB GET and POST set up

--- Backend/server.js
+++ Backend/server.js
@@ -8,15 +8,40 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-const mongoServer = "mongodb://localhost/distances"
+const mongoServer = "mongodb://localhost/actions"
 mongoose.connect(mongoServer, { useNewUrlParser: true })
 mongoose.Promise = Promise
 
+mongoose.connection.on("error", err => console.error("Connection error:", err))
+mongoose.connection.once("open", () => console.log("Connected to mongodb"))
 
+const Action = mongoose.model("Action", {
+  id: Number,
+  type: String,
+  title: String,
+  description: String,
+  co2value: 0,
+  timePeriod: 0
+})
 
+app.get("/", (req, res) => {
+  res.send("Actions API")
+})
 
+app.get("/actions/", (req, res) => {
+  Action.find().then(actions => {
+    console.log("Actions: ", solutions)
+    res.json(actions)
+  })
+})
 
+app.post("/actions", (req, res) => {
+  const action = new Action(req.body)
+  action.save()
+    .then(() => { res.status(201).send("Action added") })
+    .catch(err => { res.status(400).send(err) })
+})
 
 app.listen(8080, () =>
-  console.log("Example app listening on port 8080!")
+  console.log("Actions API listening on port 8080!")
 )

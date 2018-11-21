c4302c2a6e49a03b38a809643e0771efc3be7ebe
SaraNordell <sara@nordell.me>
2018-11-21 11:12:07 +0100

    updated frontend build placed in public folder in backend

---
+++ Backend/public/precache-manifest.8eb761c1361279529fe85e6c0a469d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "35d9dccccac6e2e2c9a8",
+    "url": "/static/js/main.35d9dccc.chunk.js"
+  },
+  {
+    "revision": "b1173878033bec9ab953",
+    "url": "/static/js/1.b1173878.chunk.js"
+  },
+  {
+    "revision": "35d9dccccac6e2e2c9a8",
+    "url": "/static/css/main.7840e341.chunk.css"
+  },
+  {
+    "revision": "6ab74d2db4fe175d85e4ea3060d53a0e",
+    "url": "/index.html"
+  }
+];

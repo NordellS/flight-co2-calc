9910b5d053d8ba96be76ac30ff6fd19b5e8ae423
Jeanette <aejlindberg@gmail.com>
2018-11-22 10:43:24 +0100

    landingpage styling in progress

--- Frontend/src/components/LandingPage/landingPage.js
+++ Frontend/src/components/LandingPage/landingPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import Footer from "../Footer/footer.js"
 import "./landingPage.scss"
 
@@ -7,12 +8,16 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div>
-      <div className="pageWrapper">
-        LANDING PAGE
-        INTRO TO OUR PROJECT
-        <img src="/globe.svg" alt="globe" />
-      </div>
-      <Footer />
+        <div className="pageWrapper">
+          <Link to="/"><h1>Green Your Conscience</h1></Link>
+          <img src="/globe.svg" alt="globe" />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis luctus nisi. Duis id ipsum ut nunc consequat faucibus in ut neque. Praesent eu consectetur tellus. Fusce et nibh volutpat, porttitor ex sed, lobortis dui. Aliquam sodales est quis dolor dignissim volutpat. Ut lacus risus, sodales sed est id, feugiat pretium velit. Vivamus sollicitudin nunc id magna viverra finibus. Pellentesque a molestie quam. Morbi ornare interdum urna. Vivamus tortor tellus, posuere eget facilisis quis, tristique vitae metus. Mauris quis ullamcorper velit.
+          Maecenas rutrum purus a erat porta tempus. Ut ac lectus velit. Maecenas condimentum sapien at quam pretium tempus. Aenean et aliquet lectus. Aliquam sit amet augue non odio venenatis aliquet. Nullam efficitur consectetur lorem feugiat ullamcorper. Ut auctor nunc quis suscipit eleifend. Vivamus id posuere massa.
+          In sollicitudin, nunc a lacinia bibendum, ante dolor auctor augue, ut volutpat nisl diam et massa. Morbi ut tellus erat. Mauris magna neque, lacinia a magna et, maximus vehicula turpis. Donec maximus velit nibh, in condimentum nunc condimentum at. Pellentesque velit tortor, pharetra quis commodo et, condimentum nec dui. Donec ac laoreet tellus, pharetra consequat nisl. Morbi tempor aliquam lobortis. Cras congue, nibh convallis pellentesque venenatis, massa magna luctus ipsum, dictum suscipit magna turpis et lorem. Nulla et velit odio. Aenean gravida efficitur ultricies.
+          </p>
+        </div>
+        <Footer />
       </div>
     )
   }

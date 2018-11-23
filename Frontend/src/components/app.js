c2dd7094932ba807641209354b15d40dbe9c7698
SaraNordell <sara@nordell.me>
2018-11-23 15:06:58 +0100

    changed to hashrouter

--- Frontend/src/components/app.js
+++ Frontend/src/components/app.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import LandingPage from "./LandingPage/landingPage"
 import FlightsPage from "./FlightsPage/flightsPage"
 import ActionsPage from "./ActionsPage/actionsPage"
